74cfe19d7a70f24cc1ba72ca6179dfdf0d77cb6d
Akram <akram.codes.it@gmail.com>
2025-01-07 12:36:10 +0530

    Enhanced the easy.sql file in HackerRank/Easy with additional solutions and improved formatting for better readability.

--- HackerRank/Easy/easy.sql
+++ HackerRank/Easy/easy.sql
@@ -84,3 +84,46 @@ SELECT DISTINCT CITY
 FROM STATION
 WHERE 
     LOWER(RIGHT(CITY, 1)) NOT IN ('a', 'e', 'i', 'o', 'u');
+
+
+-- Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY
+FROM 
+	STATION
+WHERE 
+    LOWER(RIGHT(CITY, 1)) NOT IN ('a', 'e', 'i', 'o', 'u');
+
+-- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY
+FROM 
+	STATION
+WHERE 
+    LOWER(LEFT(CITY, 1)) NOT IN ('a', 'e', 'i', 'o', 'u')
+AND
+    LOWER(RIGHT(CITY, 1)) NOT IN ('a', 'e', 'i', 'o', 'u');
+
+-- Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+SELECT Name
+FROM 
+	STUDENTS
+WHERE 
+	Marks > 75
+ORDER BY 
+	RIGHT(Name, 3), ID ASC;
+
+-- Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+SELECT name from Employee
+ORDER BY 
+name ASC;
+
+-- Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than $2000 per month who have been employees for less than 10 months. Sort your result by ascending employee_id.
+SELECT name 
+FROM
+    Employee
+WHERE 
+    salary>2000
+AND 
+    months<10
+ORDER BY
+    employee_id ASC;
+
